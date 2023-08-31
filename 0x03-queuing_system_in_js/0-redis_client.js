c58f8193eb52768e5b96359524fcb9ba9916a44f
kitts <kittsmakokha@gmail.com>
2023-08-31 20:13:41 +0000

    adding promises

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,14 +1,27 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+async function main() {
+  const client = redis.createClient();
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
+  client.on('connect', () => {
+    console.log('Redis client connected to the server');
+    client.quit(); 
+  });
+
+  client.on('error', (error) => {
+    console.error(`Redis client not connected to the server: ${error.message}`);
+    client.quit(); 
+  });
+
+  await new Promise((resolve) => {
+    client.on('end', () => {
+      console.log('Redis client connection closed');
+      resolve();
+    });
+  });
+}
+
+main().catch((error) => {
+  console.error('An error occurred:', error);
 });
 
-client.on('error', (error) => {
-  console.error(`Redis client not connected to the server: ${error.message}`);
-});
-
-
-client.quit();
